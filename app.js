1cf9d32fb7a416b884341b7c2a764da8320b1b24
Prab <prab.missier@gmail.com>
2025-01-19 23:38:03 +1030

    Initial version of app file

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+
+const portnum = process.env.PORT||2500
+
+const exphandlebars = require('express-handlebars').create({defaultLayout:'main'})
+app.engine('handlebars', exphandlebars.engine)
+app.set('view engine', 'handlebars')
+
+//app.set(express.static(__dirname +'/public'))
+
+
+app.get('/', (req,res) => {
+    res.render('home', {author : 'Prabhu'})
+})
+
+app.listen(portnum, () => {
+    console.log('Server is running')
+})
