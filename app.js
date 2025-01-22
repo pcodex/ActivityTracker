42c837a0b7476e39d8ff119d84f45b89b539b08f
Prab <prab.missier@gmail.com>
2025-01-22 23:09:31 +1030

    Used the Mongo Driver to do a prototype Create

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 
 const handlers = require('./libs/handlers')
 
+
+
 const app = express()
 
 const portnum = process.env.PORT||2000
@@ -14,11 +16,14 @@ const exphandlebars = require('express-handlebars').create({defaultLayout:'main'
 app.engine('handlebars', exphandlebars.engine)
 app.set('view engine', 'handlebars')
 
+
+
+
 //app.set(express.static(__dirname +'/public'))
 
 
 app.get('/', (req,res) => {
-    res.render('home', {author : 'Prabhu'})
+    res.render('home', {author : 'Prabhu'})    
 })
 
 app.post('/submitActivity', handlers.saveActivity)
--- libs/handlers.js
+++ libs/handlers.js
@@ -1,6 +1,39 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 exports.saveActivity = (req,res) => {
     console.log("Activity Description " + req.body.actdesc)
+    //DB stuff
+    
+    const uri = 'mongodb://localhost:27017'
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri,  {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+}
+);    
+
+    async function run() {
+        try {
+          // Connect the client to the server (optional starting in v4.7)
+          await client.connect();
+          // Send a ping to confirm a successful connection
+          await client.db("PrabActive").collection("activities").insertOne({
+            date:new Date(),
+            name:req.body.actdesc
+          })
+          console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        } finally {
+          // Ensures that the client will close when you finish/error
+          await client.close();
+        }
+      }
+      run().catch(console.dir);
+
     res.redirect(303, '/activitySubmitted')
+
 }
 
 
